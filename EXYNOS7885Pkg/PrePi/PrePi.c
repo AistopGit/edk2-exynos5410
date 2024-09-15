a3f27f54abf5978ba8b9333375cbce0413baa2ca
AistopGit <oleg18072008t@gmail.com>
2024-09-15 07:19:19 -0400

    rule britannia

--- EXYNOS7885Pkg/PrePi/PrePi.c
+++ EXYNOS7885Pkg/PrePi/PrePi.c
@@ -108,19 +108,13 @@ VOID Main (IN  UINT32  StartTimeStamp)
 
   // Now, the HOB List has been initialized, we can register performance information
   //PERF_START (NULL, "PEI", NULL, StartTimeStamp);
-  
-  DEBUG((EFI_D_INFO, "nice im stuck here please reboot me\n"));
 
   // SEC phase needs to run library constructors by hand.
   ProcessLibraryConstructorList ();
-  
-  DEBUG((EFI_D_INFO, "not expected to reach here on exynos 5410\n"));
 
   // Assume the FV that contains the SEC (our code) also contains a compressed FV.
   Status = DecompressFirstFv ();
   ASSERT_EFI_ERROR (Status);
-  
-  DEBUG((EFI_D_INFO, "and guardian angels sang this strain\n"));
 
   // Load the DXE Core and transfer control to it
   Status = LoadDxeCoreFromFv (NULL, 0);
